12fb1d2a2d34a2c50d8efc0bd041932ab8fc183c
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-03-06 16:13:39 +0100

    feature(user-oidc): Soft auto provisioning without user creation
    Source: https://github.com/nextcloud/user_oidc?tab=readme-ov-file#soft-auto-provisioning-without-user-creation
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- configs/oidc.config.php
+++ configs/oidc.config.php
@@ -8,9 +8,16 @@ $CONFIG = [
 		// > (mapped attributes) of the existing user. If the user does not
 		// > exist in another backend, we create it in the user_oidc backend
 		// https://github.com/nextcloud/user_oidc#soft-auto-provisioning
-		'auto_provision' => false,
+		'auto_provision' => true,
 		// Update *existing information* in Nextcloud backend
 		// (false = fail login if existing in other backend)
-		'soft_auto_provision' => false,
+		'soft_auto_provision' => true,
+		// > Soft auto provisioning but prevent user_oidc to create users,
+		// > meaning you want user_oidc to accept connection only for users that already exist in Nextcloud
+		// > and are managed by other user backend BUT you still want user_oidc to set the user information
+		// > according to the OIDC mapped attributes.
+		//
+		// https://github.com/nextcloud/user_oidc?tab=readme-ov-file#soft-auto-provisioning-without-user-creation
+		'disable_account_creation' => true,
 	],
 ];
