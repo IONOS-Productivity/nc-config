ca60132158292b41a86756e41f4c560d4b67718d
Tatjana Kaschperko Lindt <kaschperko-lindt@strato.de>
2025-04-24 12:05:28 +0200

    feat: add config for enabledPreviewProviders, preview_*
    they were removed from helm config
    
    Signed-off-by: Tatjana Kaschperko Lindt <kaschperko-lindt@strato.de>

---
+++ configs/preview-providers.config.php
@@ -0,0 +1,28 @@
+<?php
+
+$CONFIG = [
+	'enabledPreviewProviders' => [
+		'OC\Preview\BMP',
+		'OC\Preview\GIF',
+		'OC\Preview\HEIC',
+		'OC\Preview\Imaginary',
+		'OC\Preview\JPEG',
+		'OC\Preview\Krita',
+		'OC\Preview\MarkDown',
+		'OC\Preview\Movie',
+		'OC\Preview\MP3',
+		'OC\Preview\OpenDocument',
+		'OC\Preview\PNG',
+		'OC\Preview\SVG',
+		'OC\Preview\TIFF',
+		'OC\Preview\TXT',
+		'OC\Preview\WebP',
+		'OC\Preview\XBitmap',
+	],
+	'preview_max_x' => 8192,
+	'preview_max_y' => 8192,
+	'preview_max_filesize_image' => 256,
+	'preview_imaginary_url' => (string)getenv('IMAGINARY_HOST'),
+	'preview_imaginary_key' => (string)getenv('IMAGINARY_KEY'),
+	'preview_ffmpeg_path' => '/usr/bin/ffmpeg',
+];
