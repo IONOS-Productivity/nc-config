30185634e89fc334c7d38f52c3b20a5a22b7f920
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-07-23 14:37:34 +0200

    chore: add SPDX license information to configure script
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- configure.sh
+++ configure.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# SPDX-FileCopyrightText: 2025 STRATO GmbH
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 #===============================================================================
 # HiDrive Next Configuration Script
