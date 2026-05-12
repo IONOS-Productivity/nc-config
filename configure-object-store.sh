3afb58bedcc8a3afd746e8a2b646110299195e39
Misha M.-Kupriyanov <kupriyanov@strato.de>
2026-05-12 14:39:52 +0200

    style(configure-object-store.sh): reformat heredoc for editorconfig compliance
    Switches the write_config_file heredoc from the dash-stripping <<-EOF
    form to plain <<EOF with the body at column 0, allowing tab
    indentation for the embedded PHP. The dash-form strips ALL leading
    tabs from each line, so it forced space-only indentation inside the
    heredoc, which clashed with the new editorconfig-checker workflow's
    indent_style=tab rule for shell files.
    
    Functionally equivalent: the generated object-store.config.php now
    uses tab indentation, matching the style of every other partial
    config under IONOS/configs/.
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- configure-object-store.sh
+++ configure-object-store.sh
@@ -17,27 +17,29 @@ write_config_file() {
 	config="${NEXTCLOUD_ROOT_DIR}/config/object-store.config.php"
 
 	# Note: backslashes require double escaping due to shell (\\ -> \\\\)
-	cat >${config} <<-EOF
-		<?php
-		\$CONFIG = [
-		  // https://docs.nextcloud.com/server/latest/admin_manual/configuration_files/primary_storage.html
-		  'objectstore' => [
-		    'class' => '\\\\OC\\\\Files\\\\ObjectStore\\\\S3',
-		    'arguments' => [
-		      'autocreate' => false,
-		      'bucket' => '${ENC_OBJECT_STORAGE_BUCKET_NAME}',
-		      'region' => '${ENC_OBJECT_STORAGE_REGION}',
-		      'hostname' => '${ENC_OBJECT_STORAGE_HOSTNAME}',
-		      'port' => ${ENC_OBJECT_STORAGE_PORT},
-		      'use_ssl' => ${use_ssl_value},
-		      'key' => '${ENC_OBJECT_STORAGE_ACCESS_KEY}',
-		      'secret' => '${ENC_OBJECT_STORAGE_SECRET}',
-		      'objectPrefix' => 'urn:oid:',
-		      'use_path_style' => ${use_path_style_value},
-		    ],
-		  ],
-		];
-	EOF
+	# Heredoc body is at column 0 and uses tabs for PHP indentation to keep
+	# editorconfig-checker happy (the dash-form <<-EOF would strip those tabs).
+	cat >"${config}" <<EOF
+<?php
+\$CONFIG = [
+	// https://docs.nextcloud.com/server/latest/admin_manual/configuration_files/primary_storage.html
+	'objectstore' => [
+		'class' => '\\\\OC\\\\Files\\\\ObjectStore\\\\S3',
+		'arguments' => [
+			'autocreate' => false,
+			'bucket' => '${ENC_OBJECT_STORAGE_BUCKET_NAME}',
+			'region' => '${ENC_OBJECT_STORAGE_REGION}',
+			'hostname' => '${ENC_OBJECT_STORAGE_HOSTNAME}',
+			'port' => ${ENC_OBJECT_STORAGE_PORT},
+			'use_ssl' => ${use_ssl_value},
+			'key' => '${ENC_OBJECT_STORAGE_ACCESS_KEY}',
+			'secret' => '${ENC_OBJECT_STORAGE_SECRET}',
+			'objectPrefix' => 'urn:oid:',
+			'use_path_style' => ${use_path_style_value},
+		],
+	],
+];
+EOF
 }
 
 main() {
