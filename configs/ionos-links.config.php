0694efb522ff48e4d1378823ffb572c360226a5b
Misha M.-Kupriyanov <kupriyanov@strato.de>
2026-06-04 19:00:18 +0200

    feat(config): set market-specific IONOS URLs via config partial
    Resolve IONOS links from the MARKET env var declaratively in
    ionos-links.config.php (IIFE-scoped, ionos_peer_products nesting, DE
    fallback) instead of imperatively via config:system:set in configure.sh.
    
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- configs/ionos-links.config.php
+++ configs/ionos-links.config.php
@@ -1,23 +1,69 @@
 <?php
 
-/*
- * SPDX-FileCopyrightText: 2025 STRATO GmbH
- *
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
+$CONFIG = (static function (): array {
+	$market = strtoupper((string)getenv('MARKET'));
 
-$CONFIG = [
-	'ionos_peer_products' => [
-		'ionos_webmail_target_link' => 'https://email.ionos.fr/',
-	],
-	'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0027',
-	'ionos_customclient_windows' => 'https://wl.hidrive.com/easy/0003',
-	'ionos_customclient_macos' => 'https://wl.hidrive.com/easy/1003',
-	'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0022',
-	'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/0021',
-	'ionos_customclient_ios_appid' => '6738140194',
-	'ionos_customclient_linux' => 'https://wl.hidrive.com/easy/2003',
-	'ionos_customclient_nautilus' => 'https://wl.hidrive.com/easy/2004',
-	'ionos_homepage' => 'https://ionos.fr/',
-	'simpleSignUpLink.shown' => false,
-];
+	$marketLinks = [
+		'FR' => [
+			'ionos_peer_products' => [
+				'ionos_webmail_target_link' => 'https://email.ionos.fr/',
+			],
+			'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0027',
+			'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0022',
+			'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/0021',
+			'ionos_homepage' => 'https://ionos.fr/',
+		],
+		'DE' => [
+			'ionos_peer_products' => [
+				'ionos_webmail_target_link' => 'https://email.ionos.de/',
+			],
+			'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0047',
+			'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0002',
+			'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/0001',
+			'ionos_homepage' => 'https://ionos.de/',
+		],
+		'ES' => [
+			'ionos_peer_products' => [
+				'ionos_webmail_target_link' => 'https://email.ionos.es/',
+			],
+			'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0017',
+			'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0032',
+			'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/0031',
+			'ionos_homepage' => 'https://ionos.es/',
+		],
+		'UK' => [
+			'ionos_peer_products' => [
+				'ionos_webmail_target_link' => 'https://email.ionos.co.uk/',
+			],
+			'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0007',
+			'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0012',
+			'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/0011',
+			'ionos_homepage' => 'https://ionos.co.uk/',
+		],
+		'IT' => [
+			'ionos_peer_products' => [
+				'ionos_webmail_target_link' => 'https://email.ionos.it/',
+			],
+			'ionos_help_target_link' => 'https://wl.hidrive.com/easy/0037',
+			'ionos_customclient_android' => 'https://wl.hidrive.com/easy/0033',
+			'ionos_customclient_ios' => 'https://wl.hidrive.com/easy/00311',
+			'ionos_homepage' => 'https://ionos.it/',
+		],
+	];
+
+	if (!isset($marketLinks[$market])) {
+		if ($market !== '') {
+			error_log(sprintf('MARKET invalid (%s), defaulting to DE links', $market));
+		}
+		$market = 'DE';
+	}
+
+	return array_merge([
+		'ionos_customclient_windows' => 'https://wl.hidrive.com/easy/0003',
+		'ionos_customclient_macos' => 'https://wl.hidrive.com/easy/1003',
+		'ionos_customclient_ios_appid' => '6738140194',
+		'ionos_customclient_linux' => 'https://wl.hidrive.com/easy/2003',
+		'ionos_customclient_nautilus' => 'https://wl.hidrive.com/easy/2004',
+		'simpleSignUpLink.shown' => false,
+	], $marketLinks[$market]);
+})();
